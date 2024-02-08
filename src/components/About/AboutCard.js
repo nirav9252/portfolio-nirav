c1a1fd569d5f7e168aeb491d3b0d14ba134d460c
nirav9252 <niravmotivaras9252@gmail.com>
2024-02-08 17:24:46 +0530

    portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+const AboutCard = () => {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Nirav Motivaras </span>
+            from <span className="purple"> Ahmedabad, India.</span>
+            <br />
+            I am currently employed as a software developer at Ahmedabad.
+            <br />
+            I have completed Integrated MSc in IT in year 2022.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Workout
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          {/* <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p> */}
+          {/* <footer className="blockquote-footer">Soumyajit</footer> */}
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default AboutCard;
