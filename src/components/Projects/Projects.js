89c09e331005157273684cdb349ed000a577e2e5
nirav-motivaras <Niravmotivaras9252@gmail.com>
2025-03-28 15:24:05 +0530

    'portfolio'

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,6 +10,7 @@ import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 import hrms from "../../Assets/Projects/hrms.png";
+import buzzticks from "../../Assets/Projects/buzzticks.png";
 import organization from "../../Assets/Projects/organization.png";
 import foodcouture from "../../Assets/Projects/foodcouture.png";
 
@@ -25,6 +26,18 @@ const Projects = () => {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              // imgPath={chatify}
+              imgPath={buzzticks}
+              isBlog={false}
+              title="HRMS"
+              description="uzzTicks is an online booking platform that simplifies event planning and ticketing for a wide range of activities. The website provides tools for users to easily manage event registration, sales, and booking systems. Whether for concerts, conferences, or smaller gatherings, BuzzTicks offers customizable features to enhance the booking experience."
+              // ghLink="https://github.com/soumyajit4419/Chatify"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               // imgPath={chatify}
@@ -45,7 +58,7 @@ const Projects = () => {
               title="Organization Website"
               description="In this website you can get detail about the organization like technology, services, blog and more. You can also view opening position and apply for this designation threw this website. You can get contact information for this organization."
               // ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://www.softyoi.com/"
+              // demoLink="https://www.softyoi.com/"
             />
           </Col>
 
@@ -56,7 +69,7 @@ const Projects = () => {
               title="Food Couture"
               description="Food Couture offers to view new recipes video to create delicious food. You can purchase full courses video and also get pdf of recipes."
               // ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://foodcouture.in/"
+              // demoLink="https://foodcouture.in/"
             />
           </Col>
 
